b0fd4e9032f7bc5bff02885c70ad11211d599328
AmrtPrjpt <amrtprjpt@gmail.com>
2022-05-29 16:43:12 +0545

    refactored

--- store/tests/tests_views.py
+++ store/tests/tests_views.py
@@ -4,7 +4,7 @@ from django.test import Client, RequestFactory, TestCase
 from django.urls import reverse
 
 from store.models import Category, Product
-from store.views import all_products
+from store.views import product_all
 
 
 class TestViewResponses(TestCase):
@@ -21,6 +21,16 @@ class TestViewResponses(TestCase):
             price="20.00",
             image="django",
         )
+
+    def test_url_allowed_hosts(self):
+        """
+        Test Allowed Hosts
+
+        """
+        response = self.c.get("/", HTTP_HOST="127.0.0.1")
+        self.assertEqual(response.status_code, 200)
+        response = self.c.get("/", HTTP_HOST="yourdomain.com")
+        self.assertEqual(response.status_code, 400)
 
     def test_homepage_url(self):
         """
@@ -50,9 +60,9 @@ class TestViewResponses(TestCase):
         Example: code validation, search HTML for text
         """
         request = HttpRequest()
-        response = all_products(request)
+        response = product_all(request)
         html = response.content.decode("utf8")
-        self.assertIn("<title>Home</title>", html)
+        self.assertIn("<title>BookStore</title>", html)
         self.assertTrue(html.startswith("\n<!DOCTYPE html>\n"))
         self.assertEqual(response.status_code, 200)
 
@@ -60,9 +70,9 @@ class TestViewResponses(TestCase):
         """
         Example: Using request factory
         """
-        request = self.factory.get("/item/django-beginners")
-        response = all_products(request)
+        request = self.factory.get("/react")
+        response = product_all(request)
         html = response.content.decode("utf8")
-        self.assertIn("<title>Home</title>", html)
+        self.assertIn("<title>BookStore</title>", html)
         self.assertTrue(html.startswith("\n<!DOCTYPE html>\n"))
         self.assertEqual(response.status_code, 200)
